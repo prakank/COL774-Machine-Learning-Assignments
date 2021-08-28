c006fc80418d3ef5853d9858ff66ecd95bc72050
Prakhar Aggarwal <cs5190441@iitd.ac.in>
2021-08-28 14:16:53 +0530

    Assignment-1 variables name

--- Assignment-1/Q1/main.py
+++ Assignment-1/Q1/main.py
@@ -30,30 +30,30 @@ def normalize(data):
     data = (data - data.mean())/data.std()
     return data
 
-def gradient_loss_function(data_X, data_Y, theta): # -(1/batch_size) * summation_1_batch_size ( ( y(i) - h_theta(x_i) ) * x(i) )
+def batch_gradient(data_X, data_Y, theta): # -(1/batch_size) * summation_1_batch_size ( ( y(i) - h_theta(x_i) ) * x(i) )
     gradient = np.zeros((2))
     loss = 0
     for i in range(BATCH_SIZE):
         index = np.random.randint(100)
         x = np.array((data_X[index], 1.0))
-        partial_compute = ((data_Y[index] - h_theta(theta, x))*x).reshape(x.shape[0])
+        partial_gradient = ((data_Y[index] - h_theta(theta, x))*x).reshape(x.shape[0])
         partial_loss    = loss_function(data_Y[index], theta, x)
         
-        gradient = np.add(gradient,partial_compute)
+        gradient = np.add(gradient,partial_gradient)
         loss     = partial_loss + loss
         
     gradient = np.multiply(gradient,-1/BATCH_SIZE)
-    loss = (loss)/(BATCH_SIZE*2)
+    loss = (loss)/(2*BATCH_SIZE)
     
     return loss, gradient
 
 def converge(loss1, loss2):
     return abs(loss1 - loss2) <= DELTA 
 
-def check(theta, data_X, data_Y):
+def predict(theta, data_X, data_Y):
     for i in range(EXAMPLES):
         x = np.array((data_X[i], 1.0))
-        # print(h_theta(theta, x), data_Y[i])
+        print("x:{}, y_original:{}, y_predict:{}".format(data_X[i], data_Y[i], h_theta(theta, x)))
 
 def graph_loss(data):
     plt.plot(data)
@@ -61,8 +61,10 @@ def graph_loss(data):
     plt.title("Loss function (SGD) vs Iteration")
     plt.xlabel("Iteration Number")
     plt.ylabel("Loss function (SGD)")
-    plt.savefig("output.jpg")
-    # plt.xticks(y)    
+    
+    os.system('mkdir -p assets')
+    plt.savefig(os.path.join(BASE_DIR, 'Q1', 'assets', 'Loss_function.jpg'))
+        
     plt.show()
 
 def graph_y_yhat(data_x, data_y, theta):
@@ -88,38 +90,59 @@ def graph_y_yhat(data_x, data_y, theta):
     plt.plot([min_x, max_x], [h_theta(theta, min_x), h_theta(theta, max_x)], label='Predicted Value', color='orange')
     plt.title("Acidity vs Density")
     plt.xlabel("Acidity (Normalized)")
-    plt.ylabel("Density")
-    plt.savefig("y_yhat.jpg")
+    plt.ylabel("Density")    
+    
+    os.system('mkdir -p assets')
+    plt.savefig(os.path.join(BASE_DIR, 'Q1', 'assets', 'partB_hypothesis_function.jpg'))
+        
     plt.legend()
     plt.show()
+
+def update_params(theta, learning_rate, gradient):
+    return np.add(theta, np.multiply(gradient, -learning_rate))
+
+def partA(iterations, learning_rate, stopping_threshold, theta, loss, data_x, data_y):
+    print("Part A")
+    print("Iterations: {}".format(iterations))
+    print("Stopping Threshold: {}".format(stopping_threshold))
+    print("Parameters Learned: {}".format(theta))
     
-def linear_regression(data_x, data_y):
-    data_x = normalize(data_x)
-    if data_x.shape[0] != data_y.shape[0]:
-        raise IOError("Inconsistent data")
+    input("\nPress Enter to move to Part B")
+    partB(iterations, learning_rate, stopping_threshold, theta, loss, data_x, data_y)
+
+def partB(iterations, learning_rate, stopping_threshold, theta, loss, data_x, data_y):
+    graph_y_yhat(data_x, data_y, theta)
+    input("\nPress Enter to move to Part C")
+    partC(iterations, learning_rate, stopping_threshold, theta, loss, data_x, data_y)
+
+def partC(iterations, learning_rate, stopping_threshold, theta, loss, data_x, data_y):
+    pass
+
+def batch_gradient_descent(data_x, data_y):    
     global EXAMPLES
     EXAMPLES = data_x.shape[0]
     theta = np.zeros((2)) # Initialized theta to be a zero vector
     
-    count1, count2 = 0, 0
-    loss1, loss2 = 0, 0
-    i = 0
+    count1, count2 = 0, 0 # For checking convergence (SGD)
+    loss1, loss2   = 0, 0
     
-    data_plot = []
+    i = 0 # Iteration count
+    
+    loss = []
     
     while True:
         i+=1
-        partial_loss, gradient = gradient_loss_function(data_x, data_y, theta)
-        theta = np.add(theta, np.multiply(gradient, -learning_rate))
+        iteration_loss, gradient = batch_gradient(data_x, data_y, theta)
+        theta = update_params(theta, learning_rate, gradient)        
         
-        data_plot.append(partial_loss)
+        loss.append(iteration_loss)
         
         if count1 < K_ITERATIONS:
             count1+=1
-            loss1 += partial_loss
+            loss1 += iteration_loss
         elif  count2 < K_ITERATIONS:
             count2+=1
-            loss2 += partial_loss
+            loss2 += iteration_loss
             
         if(count2 == K_ITERATIONS):
             loss1 /= BATCH_SIZE
@@ -127,15 +150,17 @@ def linear_regression(data_x, data_y):
             count1 = 0
             count2 = 0
         
-        if converge(loss1,loss2):
-            # print("Iterations: {}".format(i))
-            print("Parameters Learned: {}".format(theta))
-            check(theta, data_x, data_y)
-            # graph_loss(data_plot)
-            graph_y_yhat(data_x, data_y, theta)
-            return
+            if converge(loss1,loss2):
+                partA(i, learning_rate, DELTA, theta, loss, data_x, data_y)
+                # print("Iterations: {}".format(i))
+                # print("Parameters Learned: {}".format(theta))
+                # graph_loss(loss)
+                graph_y_yhat(data_x, data_y, theta)
+                return
 
 if __name__ == '__main__':
-    X = data_load('linearX.csv')
+    X = normalize(data_load('linearX.csv'))
     Y = data_load('linearY.csv')
-    linear_regression(X,Y)
+    if X.shape[0] != Y.shape[0]:
+        raise IOError("Inconsistent dimensions of data")
+    batch_gradient_descent(X,Y)
